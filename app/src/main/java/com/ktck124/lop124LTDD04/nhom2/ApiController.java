88e7cbfb63ff087b583dbcb6f00b10b10bff93db
Pad2806 <tuiladuyne28@gmail.com>
2024-12-21 11:30:07 +0700

    Update

--- app/src/main/java/com/ktck124/lop124LTDD04/nhom2/ApiController.java
+++ app/src/main/java/com/ktck124/lop124LTDD04/nhom2/ApiController.java
@@ -1,12 +1,10 @@
 package com.ktck124.lop124LTDD04.nhom2;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ApiController {
-    private static final String BASE_URL = "http://192.168.1.4:8082/Baocao/";
+public class APIController {
+    private static final String BASE_URL = "http://192.168.1.4:82/CuoiKi/";
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofitInstance() {
